ae73e1b2c42ec08fa13099f22ca2820d571e81c1
vignesh <you@example.com>
2025-10-17 11:28:51 +0530

    java class issue fixed

--- jenkinsfiles_practice/src/main/java/mypractice/Helloworldapp.java
+++ jenkinsfiles_practice/src/main/java/mypractice/Helloworldapp.java
@@ -7,7 +7,7 @@ public class Helloworldapp {
 
     private static final String MESSAGE = "Hello World!";
 
-    public App() {}
+    public Helloworldapp() {}
 
     public static void main(String[] args) {
         System.out.println(MESSAGE);
