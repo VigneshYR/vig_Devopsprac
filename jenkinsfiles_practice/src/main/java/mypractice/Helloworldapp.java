0e0ced4690e9b451435600a63f414f18f7d76e2b
VigneshYR <vigneshprasadvin@gmail.com>
2025-10-18 10:35:31 +0530

    Update Helloworldapp.java

--- jenkinsfiles_practice/src/main/java/mypractice/Helloworldapp.java
+++ jenkinsfiles_practice/src/main/java/mypractice/Helloworldapp.java
@@ -7,8 +7,6 @@ public class Helloworldapp {
 
     private static final String MESSAGE = "Hello World!";
 
-    public Helloworldapp() {}
-
     public static void main(String[] args) {
         System.out.println(MESSAGE);
     }
